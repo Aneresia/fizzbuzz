e8cd776141087762f5ba6819882e0d65fb2a8a27
Constantinescu Oana <oana.constantinescu@metrosystems.net>
2020-12-08 17:24:40 +0200

    alfresco with report

--- src/main/java/com/ness/fizzbuzz/FizzBuzzCalculator.java
+++ src/main/java/com/ness/fizzbuzz/FizzBuzzCalculator.java
@@ -7,18 +7,8 @@ import java.util.List;
 public class FizzBuzzCalculator {
 
     public static void main(String args[]){
-        FizzBuzzProcessor fizzBuzzProcessor = new FizzBuzzProcessor();
-
-        //first get a list of integers
-        List<Integer>  numbers = fizzBuzzProcessor.getNumbers("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
-        //second generate fizz buzz
-        System.out.println(fizzBuzzProcessor.calculateFizzBuzz(numbers));
-
-        System.out.println(fizzBuzzProcessor.getNumbersOfRange(1,20));
-
         //generate alfresco
-        System.out.println(fizzBuzzProcessor.calculateFizzAlfresco(numbers));
-        System.out.println(fizzBuzzProcessor.getNumbersOfRangeAlfresco(1,20));
+        System.out.println(new FizzBuzzProcessor().getAlfrescoWithReport(1,20));
 
     }
 
--- src/main/java/com/ness/fizzbuzz/util/FizzBuzzProcessor.java
+++ src/main/java/com/ness/fizzbuzz/util/FizzBuzzProcessor.java
@@ -1,10 +1,15 @@
 package com.ness.fizzbuzz.util;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FizzBuzzProcessor {
+
+    private static final String STR_INTEGER = "integer";
 
     public String calculateFizzBuzz(List<Integer> numbers) {
         return numbers.stream()
@@ -32,8 +37,40 @@ public class FizzBuzzProcessor {
                 .collect(Collectors.joining(" "));
     }
 
+    public Map<String, String> getAlfrescoWithReport(int startRange, int endRange) throws NumberFormatException{
+        return createReport(getNumbersOfRangeAlfresco(startRange, endRange));
+    }
+
+    private Map<String, String> createReport(String text) {
+        Map<String, Integer> report = new HashMap<>();
+        report.put(STR_INTEGER, 0);
+
+        Stream.of(text.split(" ")).forEach(word -> {
+            if (isNumeric(word)) {
+                report.put(STR_INTEGER, report.get(STR_INTEGER) + 1);
+            } else {
+                int count = report.getOrDefault(word, 0);
+                report.put(word, count + 1);
+            }
+        });
+
+        Map<String, String> reportString = new LinkedHashMap<>();
+        report.forEach((key, value) -> reportString.put(key, String.valueOf(value)));
+        reportString.put("TEXT", text);
+        return reportString;
+    }
+
     public List<Integer> getNumbers(String numbers) throws NumberFormatException{
         String[] x = numbers.trim().split("\\s+");
         return Stream.of(x).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
